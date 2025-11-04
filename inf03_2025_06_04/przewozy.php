7301f4c8521e5b9abdc603c30b9944584f119804
Maciej Pabisiak <maciej.pabisiak.v6@o2.pl>
2025-11-04 14:43:00 +0100

    Reapply "Day: 04.11.2025"
    This reverts commit d988bbf8d73e06c557f85c9df0d4aa7283ca52d9.

---
+++ inf03_2025_06_04/przewozy.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Fima Przewozowa</title>
+</head>
+<body>
+    <div class="naglowkowy">
+        <h1>Firma przewozowa Półdarmo</h1>
+    </div>
+    <div class="nawigacyjny">
+        <a href="kw1.png">Kwerenda 1</a>
+        <a href="kw2.png">Kwerenda 2</a>
+        <a href="kw3.png">Kwerenda 3</a>
+        <a href="kw4.png">Kwerenda 4</a>
+    </div>
+    <div class="glowny">
+        <div class="lewy">
+            <h2>Zadania do wykonania</h2>
+            <table>
+                <tr> <td><b>Zadanie do wykonywania</b></td> <td><b>Data ralizacji</b></td> <td><b>Akcja</b></td> </tr>
+
+                <tr> <td>Przewóz mebli z ulicy Nawwrot 13 na ulicę Długą 8</td> <td>2019-11-06</td> <td><u>Usuń</u></td> </tr>
+                <tr> <td>Odbiór Iistów firmowych</td> <td>2019-11-18</td> <td><u>Usuń</u></td> </tr>
+                <tr> <td>Przegląd szamochodu dostawczego w serwisie</td> <td>2019-11-20</td> <td><u>Usuń</u></td> </tr>
+                <tr> <td>Przewóz pudel z osiedla Nowe Sady do firmy</td> <td>2019-11-22</td> <td><u>Usuń</u></td> </tr>
+                <tr> <td>Odbiór towarów niesortowanych</td> <td>2019-11-23</td> <td><u>Usuń</u></td> </tr>
+                <tr> <td>Przewóz mebli z ulicy Długiej 7 na ulicę Krótką 13</td> <td>2019-11-26</td> <td><u>Usuń</u></td> </tr>
+                <tr> <td>Spotkanie Firmowe</td> <td>2019-11-10</td> <td><u>Usuń</u></td> </tr>
+            </table>
+                <br>
+                Zadanie do wykonania:
+                <input type="text">
+                <br>Data ralizacji: 
+                <input type="date">
+                <button>Dodaj</button>
+        </div>
+        <div class="prawy">
+            <img src="auto.png" alt="auto firmowe">
+            <h3>Nasza specjalny</h3>
+            <ul>
+                <li>Przeprowadzki</li>
+                <li>Przewóz mebli</li>
+                <li>Przesłki Gabrytowe</li>
+                <li>Wynajem Pojazdów</li>
+                <li>Zakupy towarów</li>
+            </ul>
+        </div>
+    </div>
+    <div class="stopka">
+        <p>Stronę przygotował: Maciej Pabisiak</p>
+    </div>
+</body>
+</html>
---
+++ inf03_2025_06_12/medica.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Przychodnia Medica</title>
+</head>
+<body>
+    <div class="naglowkowy">
+        <h1>Abonamenty w przychodni Medica</h1>
+    </div>
+    <div class="artykulu">
+        <img src="obraz1.jpg" alt="">
+        skrypt 1
+        <a href="opis.html">Dowiedz się wieciej</a>
+    </div>
+    <div class="glowny">
+        
+        <div class="pierwsza">
+            <h2>Standardowy</h2>
+            skrypt 2
+        </div>
+        
+
+        <div class="druga">
+            <h2>Premium</h2>
+            skrypt 2
+        </div>
+        
+
+        <div class="trzecia">
+            <h2>Dziecko</h2>
+            skrypt 2
+        </div>
+
+    </div>
+
+    <div class="stopki">
+        <img src="obraz2.png" alt="przychodnia">
+        <p>Stronę przygotował: Maciej Pabisiak</p>
+    </div>
+</body>
+</html>
